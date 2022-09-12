1079917f3ca1b1e75ff9b44b1ce242a06ac3f1f8
Thu Rein <rrthurein@gmail.com>
2022-09-11 17:06:13 -0700

    addCompleteSop function added

--- src/App.js
+++ src/App.js
@@ -30,49 +30,68 @@ const startSop = [
 function App() {
   const [sop, setSop] = useState(startSop)
   const [toDo, setToDo] = useState('') //state is the information that the page is rendering
+  const [completedSops, setCompletedSops] = useState([])
 
   const handleChange = (e) => {
     setToDo(e.target.value)
   }
 
   const handleAdd = () => {
-    const newSop = sop.concat({toDo})
-        setSop(newSop);
+
+    const id = Math.max(...sop.map((t) => t.id)) + 1
+    //this creates a new ID for the object that the add button creates. map: return the element you do want
+    //whenever handleAdd add one make sure to give it an id that is a valid id that non of the other item have
+    const newSop = sop.concat({toDo, id})
+     setSop(newSop);
+
+  }
+
+  const addCompletedSop = (item) => {
+    const newCompletedSop = completedSops.concat(item)
+      // completedSops.push(item)
+      setCompletedSops(newCompletedSop)
+      console.log("added", completedSops)
   }
 
   const remove = (id) => {
-    const newSopFilter = sop.filter((item)=>{
+    const newSopFilter = sop.filter((item)=>{   //filter element
       if(item.id === id){
-       return false;
+        return false
       }
       return true
     })
     setSop(newSopFilter)
   }
 
-  const uID = () => {
-    const randomID = { id : Math.floor(Math.random() * 10) }
-    return randomID;
-  }
 
-  //whenever handleAdd add one make sure to give it an id that is a valid id that non of the other item have
-  // add ID into the element
-//every time i add an object and incremented
-//object oriented programming  checklist.add.item
   //remove the item from array
-  //filter element
-  //map return the element you do want
-//key={item.id}
-//whenever you need to pass an argument to a fucntion that you are calling from event handler you need to put it in a an
-  const sops = sop.map((item) => (
-     <li>
-     <input type="checkbox" onChange={()=>{
-       console.log("Anonymous")
-       remove(item.id);
-     }}/>
-     {item.toDo}</li>)); //creating html here and rendering down
 
-console.log("rendering");
+
+//whenever you need to pass an argument to a fucntion that you are calling from event handler you need to put it in a an
+//the map loops through each item and then
+  const sops = sop.map((item) => {
+    return (
+       <li key={item.id}>
+         <input type="checkbox" onChange={()=>{
+           remove(item.id)
+           addCompletedSop(item);
+         }}/>
+         {item.toDo}
+       </li>
+     )
+   });
+
+   const completedSopList = completedSops.map((item) =>{
+       return (
+         <li>
+         {item.id}
+         {item.toDo}
+         </li>
+       )
+     })
+     //creating html here and rendering down
+
+console.log("rendering", completedSopList);
 
 
 
@@ -89,13 +108,18 @@ console.log("rendering");
           {
             sops
           }
-          {
 
-          }
           </ul>
 
       </header>
-
+    <div className="container-completedSOP">
+    <h1>Completed SOP list for the Brewery</h1>
+      <div id="completedSOP">
+      {
+        completedSopList
+      }
+      </div>
+    </div>
 
     </div>
   );
