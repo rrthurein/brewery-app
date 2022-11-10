93212fba4b5ff4aa72fa6688145bd0025c92257f
definetlyThu <rrthurein@gmail.com>
2022-11-10 15:31:10 -0800

    refactored

--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+import { gapi } from "gapi-script";
+
 const schedulingParameters = {
     lager: {
       primaryFermentation: 12,
@@ -27,40 +29,45 @@ console.log(date)
 const calculateBrewingSchedule = (brewDate, beerType) => {
   if(beerType == "lager") {
    let primaryFermentationCurrentDate = brewDate.setDate(brewDate.getDate() + schedulingParameters.lager.primaryFermentation )
-   let newDatePrimaryFermentation = new Date(primaryFermentationCurrentDate)
-   let dRestDate = newDatePrimaryFermentation.setDate(newDatePrimaryFermentation.getDate() + schedulingParameters.lager.dRest)
-   let newDRestDate = new Date(dRestDate)
-   let lageringDate = newDRestDate.setDate(newDRestDate.getDate() + schedulingParameters.lager.lagering)
-   let newLageringDate = new Date(lageringDate)
-   let carbonationDate = newLageringDate.setDate(newLageringDate.getDate() + schedulingParameters.lager.carbonation)
+   let endDatePrimaryFermentation = new Date(primaryFermentationCurrentDate)
+   let dRestDate = endDatePrimaryFermentation.setDate(endDatePrimaryFermentation.getDate() + schedulingParameters.lager.dRest)
+   let endDRestDate = new Date(dRestDate)
+   let lageringDate = endDRestDate.setDate(endDRestDate.getDate() + schedulingParameters.lager.lagering)
+   let endDateLagering = new Date(lageringDate)
+   let carbonationDate = endDateLagering.setDate(endDateLagering.getDate() + schedulingParameters.lager.carbonation)
    let endDate = new Date(carbonationDate)
-   console.log("doneDate", endDate )
+   console.log("lagerDoneDate", endDate )
    return endDate;
  } else if (beerType == "ale") {
    let primaryFermentationCurrentDate = brewDate.setDate(brewDate.getDate() + schedulingParameters.ale.primaryFermentation )
-   let newDatePrimaryFermentation = new Date(primaryFermentationCurrentDate)
-   let dRestDate = newDatePrimaryFermentation.setDate(newDatePrimaryFermentation.getDate() + schedulingParameters.ale.dRest)
-   let newDRestDate = new Date(dRestDate)
-   let coldCrashDate = newDRestDate.setDate(newDRestDate.getDate() + schedulingParameters.ale.coldCrash)
-   let newColdCrashDate = new Date(coldCrashDate)
-   let carbonationDate = newColdCrashDate.setDate(newColdCrashDate.getDate() + schedulingParameters.ale.carbonation)
+   let endPrimaryFermentation = new Date(primaryFermentationCurrentDate)
+   let dRestDate = endPrimaryFermentation.setDate(endPrimaryFermentation.getDate() + schedulingParameters.ale.dRest)
+   let endDRestDate = new Date(dRestDate)
+   let coldCrashDate = endDRestDate.setDate(endDRestDate.getDate() + schedulingParameters.ale.coldCrash)
+   let endColdCrashDate = new Date(coldCrashDate)
+   let carbonationDate = endColdCrashDate.setDate(endColdCrashDate.getDate() + schedulingParameters.ale.carbonation)
    let endDate = new Date(carbonationDate)
-   console.log("doneDate", endDate )
+   console.log("aleDoneDate", endDate )
    return endDate;
  } else if (beerType == "kettleSour"){
    let souringWortDate = brewDate.setDate(brewDate.getDate() + schedulingParameters.kettleSour.primaryFermentation )
-   let souringWortNewDate = new Date(souringWortDate)
-   let primaryFermentationCurrentDate = souringWortNewDate.setDate(souringWortNewDate.getDate() + schedulingParameters.kettleSour.primaryFermentation )
-   let newDatePrimaryFermentation = new Date(primaryFermentationCurrentDate)
-   let dRestDate = newDatePrimaryFermentation.setDate(newDatePrimaryFermentation.getDate() + schedulingParameters.kettleSour.dRest)
-   let newDRestDate = new Date(dRestDate)
-   let coldCrashDate = newDRestDate.setDate(newDRestDate.getDate() + schedulingParameters.kettleSour.coldCrash)
-   let newColdCrashDate = new Date(coldCrashDate)
-   let carbonationDate = newColdCrashDate.setDate(newColdCrashDate.getDate() + schedulingParameters.kettleSour.carbonation)
+   let endSouringWortDate = new Date(souringWortDate)
+   let primaryFermentationCurrentDate = endSouringWortDate.setDate(endSouringWortDate.getDate() + schedulingParameters.kettleSour.primaryFermentation )
+   let endPrimaryFermentation = new Date(primaryFermentationCurrentDate)
+   let dRestDate = endPrimaryFermentation.setDate(endPrimaryFermentation.getDate() + schedulingParameters.kettleSour.dRest)
+   let endDRestDate = new Date(dRestDate)
+   let coldCrashDate = endDRestDate.setDate(endDRestDate.getDate() + schedulingParameters.kettleSour.coldCrash)
+   let endColdCrashDate = new Date(coldCrashDate)
+   let carbonationDate = endColdCrashDate.setDate(endColdCrashDate.getDate() + schedulingParameters.kettleSour.carbonation)
    let endDate = new Date(carbonationDate)
-   console.log("doneDate", endDate )
+   console.log("kettleSourDoneDate", endDate )
    return endDate;
  }
 }
-// console.log(calculateBrewingSchedule(date, "lager"))
+
 calculateBrewingSchedule(date, "kettleSour")
+//
+// const calendarID = process.env.REACT_APP_CALENDAR_ID;
+// const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+// const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN;
+//
